f5873da338f04a43cb6fa5f58236f0447c88ff1c
Siddharth6780 <siddharth6780@gmail.com>
2021-10-28 12:29:19 +0530

    Customized Container Component

--- fronend/src/components/Container.js
+++ fronend/src/components/Container.js
@@ -3,7 +3,29 @@ import React from "react";
 const Container = () => {
   return (
     <>
-      
+      <div className="d-flex">
+        <div className="col-md-3">
+          <div className="card my-3">
+            <div className="card-body">
+              <div className="d-flex flex-column">
+                <h4 className="card-title">Rahul Events</h4>
+              </div>
+              <div className="d-flex flex-column">
+                <p className="card-text"><b>Purpose</b>: Marriage</p>
+                <p className="card-text"><b>Name</b>: Rahul Singh</p>
+                <p className="card-text"><b>Address</b>: Ranchi,Jharkahnd</p>
+                <p className="card-text"><b>Phone No.</b>: 9342316782</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+
+
+
+  
     </>
   );
 };

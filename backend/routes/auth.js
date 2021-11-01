b54748572e75d56edad5789d2a8e4817d967edc9
Siddharth6780 <siddharth6780@gmail.com>
2021-11-01 17:10:18 +0530

    Added JWT Auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Auth = require("../models/Auth");
 const { body, validationResult } = require("express-validator");
-var bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_TOKEN =
+  "ed6e3406efa8d18ff6b78c3f552fcbb950984a0b32a9d50945a5984591600fc5121516018e9a1eca4f10fe133e8ce91544841e24b0b7750911c1e3b6c13a4d96";
 
 router.post(
   "/signup",
@@ -18,10 +22,21 @@ router.post(
     try {
       var salt = await bcrypt.genSalt();
       var hasedPassword = await bcrypt.hash(req.body.password, salt);
-      await Auth.create({
+      const user  = await Auth.create({
         email: req.body.email,
         password: hasedPassword,
-      }).then((user) => res.json(user));
+      })
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const jwtData = await jwt.sign(data, JWT_TOKEN);
+      console.log(jwtData);
+      res.status(200).send({jwtData});
+
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error occured");
@@ -32,6 +47,7 @@ router.post(
 router.post("/login", async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
+
   try {
     let user = await Auth.findOne({ email });
     if (!user) {
